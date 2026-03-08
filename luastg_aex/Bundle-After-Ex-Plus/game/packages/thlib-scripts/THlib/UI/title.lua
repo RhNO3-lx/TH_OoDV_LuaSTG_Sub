2ccf505862ece1d9ac2c759dc56f053e77676d6b
IbukiSuika <3196486451@qq.com>
2026-03-08 12:06:17 +0800

    调整标题界面曲的循环设置

--- luastg_aex/Bundle-After-Ex-Plus/game/packages/thlib-scripts/THlib/UI/title.lua
+++ luastg_aex/Bundle-After-Ex-Plus/game/packages/thlib-scripts/THlib/UI/title.lua
@@ -98,7 +98,7 @@ end
 function stage_quit:render()
 end
 
-MusicRecord("menu", 'THlib/music/title.ogg', 87.8, 79.26)
+MusicRecord("menu", 'THlib/music/title.ogg', 126.171, 126.171)
 MusicRecord("spellcard", 'THlib/music/spellcard.ogg', 75, 0xc36e80 / 44100 / 4)
 
 stage_menu = stage.New('menu', false, true)
