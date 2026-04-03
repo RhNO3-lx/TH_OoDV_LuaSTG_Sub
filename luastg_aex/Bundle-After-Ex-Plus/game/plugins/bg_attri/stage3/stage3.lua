0aff8c7f882cd947fd90d1467c53ed3693e80d30
zhan_ding <1074437951@qq.com>
2026-04-03 10:49:41 +0800

    stage3背景

--- luastg_aex/Bundle-After-Ex-Plus/game/plugins/bg_attri/stage3/stage3.lua
+++ luastg_aex/Bundle-After-Ex-Plus/game/plugins/bg_attri/stage3/stage3.lua
@@ -2,10 +2,16 @@ stage3_bg=Class(object)
 
 local dur_sta1 = 100
 local t_sta2 = 0 + dur_sta1
-local dur_sta2 = 500
+local dur_sta2 = 100--500
 local t_sta3 = t_sta2 + dur_sta2
-local dur_sta3 = 1000
+local dur_sta3 = 100--1000
 local t_sta4 = t_sta3 + dur_sta3
+local dur_sta4 = 5000
+local t_sta5 = t_sta4 + dur_sta4
+--statu1:forest
+--statu2:rush out forest
+--statu3:rock and dark
+--statu4:pyramid
 
 function stage3_bg:init()
 
@@ -25,12 +31,15 @@ function stage3_bg:init()
     
     self.timer=0
 
-    self.z=0
+    self.x=0
     self.y=0
+    self.z=0
     
     self.gr_z=0
 
-    self.speed=0.01
+    self.speedx=0
+    self.speedy=0
+    self.speedz=0.01
 
     self.statu = 0
 
@@ -46,6 +55,12 @@ function stage3_bg:init()
     self.tree1itv = 0.6
     self.tree1num = 14
     self.tree1dx = {}
+
+    self.holex = -2
+    self.holey = -2
+    self.resetpos = false
+
+    self.rot = 0
     for i=1,self.tree1num do
         self.tree1dx[i] = (math.random() - 0.5)*1.5
         while math.abs(self.tree1dx[i]) < 0.2 do
@@ -68,34 +83,64 @@ function stage3_bg:frame()
     if self.timer >= 0 and self.timer < t_sta2 then
         self.statu = 1
     elseif self.timer >= t_sta2 and self.timer < t_sta3 then
+        local t = self.timer - t_sta2
         self.statu = 2
-        self.speed = 0.03
-        -- if self.edy < 0 then
-        --     self.edy = self.edy + 0.01
-        --     self.etdy = -2 / self.edy
-        --     if self.edy > 0 then
-        --         self.edy = 0
-        --         self.etdy = 1
-        --         self.etdz = 0
-        --     end
-        --     Set3D('at',0,self.edy,self.edz) 
-        --     Set3D('up',0,self.etdy,self.etdz)
-        -- end
-        if self.y > -0.25 then
-            self.y = self.y - 0.01
+        self.speedz = 0.03
+        if self.edy < -0.6 then
+            self.edy = self.edy + 0.01
+            self.etdy = -2 / self.edy
+            if self.edy > 0 then
+                self.edy = 0
+                self.etdy = 1
+                self.etdz = 0
+            end
+            Set3D('at',0,self.edy,self.edz) 
+            Set3D('up',0,self.etdy,self.etdz)
+        end
+        if t < 100 then
+            self.speedy = -(50-math.abs(t-50) + 1)*0.0002
+            
         end
         if self.timer >= t_sta2 + 300 then
             Set3D("fog",0,(t_sta3 - self.timer)/33,Color(200,0,0,0))
         end
         
     elseif self.timer >= t_sta3 and self.timer <= t_sta4 then
-        Set3D("fog",min((self.timer-t_sta3)/100,5),min((self.timer-t_sta3)/40,6),Color(200,0,0,0))
-        -- Set3D('at',0,-1,1)
-        -- Set3D('up',0,1,1)
+        if not self.resetpos then
+            self.x = 0
+            self.y = 0
+            self.z = 0
+            self.resetpos = true
+        end
+        local t = self.timer - t_sta3
+        Set3D("fog",min(t/100,1.8),min(t/30,6),Color(200,math.min(max(t-80,0),100),math.min(max(t-80,0),100),math.min(max(t-80,0),100)))
+        Set3D('at',0,-0.5,1)
+        Set3D('up',0,2,1)
         self.statu = 3
-        self.speed = 0
+        self.speedz = 0
+        self.speedy = -0.001
+        self.speedx = -0.001
+        if t > 900 then
+            self.speedx = -((1000-t)/100)*0.001
+            self.speedy = -((1000-t)/100)*0.001
+        end
+        self.holex = self.holex-self.speedx
+        self.holey = self.holey-self.speedy
+    elseif self.timer >= t_sta4 and self.timer <= t_sta5 then
+        Set3D('at',0,0,1) 
+        Set3D('up',0,1,0)
+        Set3D("fog",0,6,Color(200,0,0,0))
+        self.statu = 4
+        local t = self.timer - t_sta3
+        self.speedz = 0
+        self.speedy = 0
+        self.speedx = 0
+        self.rot = math.mod(self.rot + 0.01,math.pi/2)
     end
-	self.z=self.z+self.speed
+    
+    self.x=self.x+self.speedx
+    self.y=self.y+self.speedy
+	self.z=self.z+self.speedz
 end
 
 local function draw_tree1(x,y,z,num,itv)
@@ -115,6 +160,59 @@ local function draw_tree1(x,y,z,num,itv)
                             -0.175+dx,-0.5+y,dz,
                             0.175+dx,-0.5+y,dz)
     end
+end
+local function draw_pyramid(vertex,h,w,rot)
+    local radiu = math.sqrt(2)*(w/2)
+    local p1 = {(vertex[1]+radiu*math.cos(rot)),(vertex[2]-(h/2)),(vertex[3]+radiu*math.sin(rot))}
+    local P1 = {(vertex[1]+2*radiu*math.cos(rot)),(vertex[2]-h),(vertex[3]+2*radiu*math.sin(rot))}
+    local p2 = {(vertex[1]-radiu*math.sin(rot)),(vertex[2]-(h/2)),(vertex[3]+radiu*math.cos(rot))}
+    local P2 = {(vertex[1]-2*radiu*math.sin(rot)),(vertex[2]-h),(vertex[3]+2*radiu*math.cos(rot))}
+    local p3 = {(vertex[1]-radiu*math.cos(rot)),(vertex[2]-(h/2)),(vertex[3]-radiu*math.sin(rot))}
+    local P3 = {(vertex[1]-2*radiu*math.cos(rot)),(vertex[2]-h),(vertex[3]-2*radiu*math.sin(rot))}
+    local p4 = {(vertex[1]+radiu*math.sin(rot)),(vertex[2]-(h/2)),(vertex[3]-radiu*math.cos(rot))}
+    local P4 = {(vertex[1]+2*radiu*math.sin(rot)),(vertex[2]-h),(vertex[3]-2*radiu*math.cos(rot))}
+    local p = {p1,p2,p3,p4}
+    local v_ = {}
+    local v = {}
+    RenderTexture("pyramid",'',{vertex[1],vertex[2],vertex[3],256,0,Color(255,255,255,255)},
+                               {vertex[1],vertex[2],vertex[3],256,0,Color(255,255,255,255)},
+                               {p[1][1],p[1][2],p[1][3],0,512,Color(255,255,255,255)},--p[1]
+                               {p2[1],p2[2],p2[3],512,512,Color(255,255,255,255)})--p2
+    --右减左
+    v_ = {p2[1]-p[1][1],p2[3]-p[1][3]}
+    v = {v_[2]/(v_[2]^2+v_[1]^2),-v_[1]/(v_[2]^2+v_[1]^2)}
+                               
+    RenderTexture("pyramid",'',{p[1][1],p[1][2],p[1][3],0,0,Color(255,255,255,255)},
+                               {p2[1],p2[2],p2[3],512,0,Color(255,255,255,255)},
+                               {p2[1]+v[1]*w/2,(vertex[2]-h),p2[3]+v[2]*w/2,512,512,Color(255,255,255,255)},
+                               {p[1][1]+v[1]*w/2,(vertex[2]-h),p[1][3]+v[2]*w/2,0,512,Color(255,255,255,255)})
+    RenderTexture("pyramid",'',{p[1][1],p[1][2],p[1][3],512,0,Color(255,255,255,255)},
+                               {p[1][1],p[1][2],p[1][3],512,0,Color(255,255,255,255)},
+                               {p[1][1]+v[1]*w/2,(vertex[2]-h),p[1][3]+v[2]*w/2,512,512,Color(255,255,255,255)},
+                               {P1[1],(vertex[2]-h),P1[3],256,512,Color(255,255,255,255)})
+    RenderTexture("pyramid",'',{p2[1],p2[2],p2[3],0,0,Color(255,255,255,255)},
+                               {p2[1],p2[2],p2[3],0,0,Color(255,255,255,255)},
+                               {p2[1]+v[1]*w/2,(vertex[2]-h),p2[3]+v[2]*w/2,0,512,Color(255,255,255,255)},
+                               {P2[1],(vertex[2]-h),P2[3],256,512,Color(255,255,255,255)})
+
+
+
+    -- RenderTexture("pyramid",'',{vertex[1],vertex[2],vertex[3],256,0,Color(255,255,255,255)},
+    --                            {vertex[1],vertex[2],vertex[3],256,0,Color(255,255,255,255)},
+    --                            {p3[1],p3[2],p3[3],512,512,Color(255,255,255,255)},--p3
+    --                            {p2[1],p2[2],p2[3],0,512,Color(255,255,255,255)})--p2
+
+
+    -- RenderTexture("pyramid",'',{vertex[1],vertex[2],vertex[3],256,0,Color(255,255,255,255)},
+    --                            {vertex[1],vertex[2],vertex[3],256,0,Color(255,255,255,255)},
+    --                            {(vertex[1]+radiu*math.sin(rot)),(vertex[2]-(h/2)),(vertex[3]-radiu*math.cos(rot)),0,512,Color(255,255,255,255)},--p4
+    --                            {(vertex[1]+radiu*math.cos(rot)),(vertex[2]-(h/2)),(vertex[3]+radiu*math.sin(rot)),512,512,Color(255,255,255,255)})--p1
+
+
+    -- RenderTexture("pyramid",'',{vertex[1],vertex[2],vertex[3],256,0,Color(255,255,255,255)},
+    --                            {vertex[1],vertex[2],vertex[3],256,0,Color(255,255,255,255)},
+    --                            {(vertex[1]-radiu*math.cos(rot)),(vertex[2]-(h/2)),(vertex[3]-radiu*math.sin(rot)),0,512,Color(255,255,255,255)},--p3
+    --                            {(vertex[1]+radiu*math.sin(rot)),(vertex[2]-(h/2)),(vertex[3]-radiu*math.cos(rot)),512,512,Color(255,255,255,255)})--p4
 end
 
 function stage3_bg:render() 
@@ -165,11 +263,11 @@ function stage3_bg:render()
         if self.z/self.tree1itv > self.dzr then
             local dx = (math.random() - 0.5)*1.5
             while math.abs(dx) < 0.2 do
-                print(dx)
+                -- print(dx)
                 dx = (math.random() - 0.5)*1.5
             end
-            print(dx)
-            print("end")
+            -- print(dx)
+            -- print("end")
             table.insert(self.tree1dx,dx)
             table.remove(self.tree1dx,1)
             self.dzr = self.dzr + 1
@@ -220,11 +318,11 @@ function stage3_bg:render()
         if self.z/self.tree1itv > self.dzr then
             local dx = (math.random() - 0.5)*1.5
             while math.abs(dx) < 0.2 do
-                print(dx)
+                -- print(dx)
                 dx = (math.random() - 0.5)*1.5
             end
-            print(dx)
-            print("end")
+            -- print(dx)
+            -- print("end")
             table.insert(self.tree1dx,dx)
             table.remove(self.tree1dx,1)
             self.dzr = self.dzr + 1
@@ -233,33 +331,32 @@ function stage3_bg:render()
         draw_tree1(self.tree1dx,dy,dz,self.tree1num-1,self.tree1itv)
     end
     if statu == 3 then
+        local dx=-math.mod(self.x,1)
+        local dy=-math.mod(self.y,1)
+        local dz=-self.z
         local x_off = -5
         for i = 0,10  do
             local y_off = -3
             for j = 0,6 do
-                Render4V("rock", x_off-0.5, y_off+0.5, 3,
-                                x_off+0.5, y_off+0.5, 3,
-                                x_off+0.5, y_off-0.5, 3,
-                                x_off-0.5, y_off-0.5, 3)
+                Render4V("rock", x_off-0.5+dx, y_off+0.5+dy, 2+dz,
+                                x_off+0.5+dx, y_off+0.5+dy, 2+dz,
+                                x_off+0.5+dx, y_off-0.5+dy, 2+dz,
+                                x_off-0.5+dx, y_off-0.5+dy, 2+dz)
                 y_off = y_off + 1
             end
             x_off = x_off + 1
         end
+        Render4V("rock_door", -0.5+self.holex,0.5+self.holey, 2+dz,
+                        0.5+self.holex, 0.5+self.holey, 2+dz,
+                        0.5+self.holex, -0.5+self.holey, 2+dz,
+                        -0.5+self.holex, -0.5+self.holey, 2+dz)
+        -- print(dx)
+        -- print("door_x:"..self.holex.."door_y"..self.holey)
     end
     if statu == 4 then
-        local x_off = -5
-        for i = 0,10  do
-            local y_off = -3
-            for j = 0,6 do
-                Render4V("rock", x_off-0.5, y_off+0.5, 6,
-                                x_off+0.5, y_off+0.5, 6,
-                                x_off+0.5, y_off-0.5, 6,
-                                x_off-0.5, y_off-0.5, 6)
-                y_off = y_off + 1
-            end
-            x_off = x_off + 1
-        end
+        draw_pyramid({0,0.5,1.5},1.2,1,self.rot)
     end
+
 	background.WarpEffectApply()
     SetViewMode("world")
 
